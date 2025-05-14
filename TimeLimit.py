68641686c01278ee35ee090272ce7b99e52b3094
莱斯男孩（Github: LesBoys） <lesboyspp43@outlook.com>
2025-05-14 20:13:56 +0800

    +ACTION: Create+ add custom timeout handler

--- TimeLimit.py
+++ TimeLimit.py
@@ -7,16 +7,23 @@ class TimeLimit:
         self.lim = lim
         self.start = 0
         self.playing = False
+        self.custom_handler = None
     def checkpoint(self) -> bool:
         """检查点"""
         if not self.playing:
             return True
         if get_ms() - self.start > self.lim:
             self.playing = False
-            raise TimeoutError("任务已超时")
+            if self.custom_handler:
+                self.custom_handler()
+            else:
+                raise TimeoutError("任务已超时")
             self.__exit__()
             return False
         return True
+    def set_custom_handler(self, handler: callable) -> None:
+        """设置自定义处理程序"""
+        self.custom_handler = handler
     def __enter__(self) -> "TimeLimit":
         self.playing = True
         self.start = get_ms()
